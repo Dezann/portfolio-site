84af57b714492c031a5cea9b93be4775f0da1cb1
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-09-12 08:58:20 +0200

    Center Carousel in Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,26 +4,25 @@ import cardman from "../media/Projects/cardman.svg"
 import "./Projects.scss"
 import 'pure-react-carousel/dist/react-carousel.es.css';
 
-
 class Projects extends Component {
     state = {}
+
     render() {
         return (
             <div className="projects-page-content">
-                <CarouselProvider
-                    naturalSlideWidth={100}
-                    naturalSlideHeight={425}
-                    totalSlides={3}
-                    dragEnabled={true}
-                >
-                    <Slider>
-                        <Slide index={0}><img src={cardman}></img></Slide>
-                        <Slide index={1}><img src={cardman}></img></Slide>
-                        <Slide index={2}><img src={cardman}></img></Slide>
-                    </Slider>
-                    <ButtonBack>Back</ButtonBack>
-                    <ButtonNext>Next</ButtonNext>
-                </CarouselProvider>
+                <div className="carousel-wrapper">
+                    <CarouselProvider
+                        naturalSlideWidth={100}
+                        naturalSlideHeight={225}
+                        totalSlides={3}
+                    >
+                        <Slider>
+                            <Slide index={0}><img src={cardman}></img></Slide>
+                            <Slide index={1}><img src={cardman}></img></Slide>
+                            <Slide index={2}><img src={cardman}></img></Slide>
+                        </Slider>
+                    </CarouselProvider>
+                </div>
             </div >
         );
     }
