a7b4ca00afa9593e010b43535c5d989aacdd9bff
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-09-14 08:28:02 +0200

    Make Projects.js on mobile

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { CarouselProvider, Slider, Slide, DotGroup } from 'pure-react-carousel';
 import cardman from "../media/Projects/cardman.svg"
 import "./Projects.scss"
 import 'pure-react-carousel/dist/react-carousel.es.css';
@@ -16,10 +16,23 @@ class Projects extends Component {
                         naturalSlideHeight={225}
                         totalSlides={3}
                     >
+                        <DotGroup />
                         <Slider>
-                            <Slide index={0}><img src={cardman}></img></Slide>
-                            <Slide index={1}><img src={cardman}></img></Slide>
-                            <Slide index={2}><img src={cardman}></img></Slide>
+                            <Slide index={0}>
+                                <img src={cardman}></img>
+                                <p className="project-title">Title</p>
+                                <p className="project-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non ante vel velit convallis pulvinar. Sed condimentum mi sit amet nisl blandit dignissim vel vel nibh. Donec ultrices faucibus eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam facilisis augue sed iaculis congue. Curabitur tempor at risus non placerat.</p>
+                            </Slide>
+                            <Slide index={1}>
+                                <img src={cardman}></img>
+                                <p className="project-title">Title</p>
+                                <p className="project-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non ante vel velit convallis pulvinar. Sed condimentum mi sit amet nisl blandit dignissim vel vel nibh. Donec ultrices faucibus eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam facilisis augue sed iaculis congue. Curabitur tempor at risus non placerat.</p>
+                            </Slide>
+                            <Slide index={2}>
+                                <img src={cardman}></img>
+                                <p className="project-title">Title</p>
+                                <p className="project-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce non ante vel velit convallis pulvinar. Sed condimentum mi sit amet nisl blandit dignissim vel vel nibh. Donec ultrices faucibus eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam facilisis augue sed iaculis congue. Curabitur tempor at risus non placerat.</p>
+                            </Slide>
                         </Slider>
                     </CarouselProvider>
                 </div>
