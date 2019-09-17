9e0fda9f3424e3a880a75d17ce750a006928482d
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-09-17 08:32:44 +0200

    Make new mobile WelcomePage.js

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import "./WelcomePage.scss"
-import panorama from "../media/panorama.svg"
 class WelcomePage extends Component {
     state = {}
     render() {
         return (
             <div className="welcome-page-content">
-                <div className="middle-text">
-                    <div className="name">
-                        <p>Bartosz Kępka</p>
-                        <div className="line"></div>
-                    </div>
-                    <div className="job">
-                        <p>Front-end developer</p>
-                        <p>Warsaw, Poland</p>
+                <div className="dark-bg">
+                    <div className="middle-text">
+                        <div className="name">
+                            <p>Bartosz Kępka</p>
+                            <div className="line"></div>
+                        </div>
+                        <div className="job">
+                            <p>Front-end developer</p>
+                            <p>Warsaw, Poland</p>
+                        </div>
                     </div>
                 </div>
-                <img src={panorama} className="panorama"></img>
             </div>
         );
     }
