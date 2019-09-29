a5c6712a1358e6fb477fc02cf596dc9bdff8bab9
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-09-29 20:04:39 +0200

    Make menu buttons to change on hover and rewrite some css

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,46 +2,63 @@ import React, { Component } from 'react'
 import { Link } from 'react-scroll'
 import './Menu.scss'
 
+
+
+
 class Menu extends Component {
-  state = {}
-  render() {
-    return (
-      <div className='menu' id='menu'>
-        <div className='menu-pages-wrapper'>
-          <Link
-            activeClass='active'
-            to='about'
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-          >
-            About
+    constructor(props) {
+        super(props)
+        this.sections = React.createRef()
+    }
+    state = {
+    }
+
+    detectSection = () => {
+        let sections = this.sections.current
+        console.log(sections)
+    }
+
+    componentDidMount() {
+        this.detectSection()
+    }
+    render() {
+        return (
+            <div className='menu' id='menu'>
+                <div className='menu-pages-wrapper' ref={this.sections}>
+                    <Link
+                        activeClass='active'
+                        to='about'
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                    >
+                        About
           </Link>
-          <Link
-            activeClass='active'
-            to='projects'
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-          >
-            Projects
+                    <Link
+                        activeClass='active'
+                        to='projects'
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                    >
+                        Projects
           </Link>
-          <Link
-            activeClass='active'
-            to='contact'
-            spy={true}
-            smooth={true}
-            offset={-70}
-            duration={500}
-          >
-            Contact
+                    <Link
+                        activeClass='active'
+                        to='contact'
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={500}
+                    >
+                        Contact
           </Link>
-        </div>
-      </div>
-    )
-  }
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Menu
