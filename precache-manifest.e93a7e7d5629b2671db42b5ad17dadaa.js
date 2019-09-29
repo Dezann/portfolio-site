39c6eca1f0079f6395a35366d196cc1ae1a18051
Bartosz Kępka <bartoszkepka1999@gmail.com>
2019-09-29 20:22:35 +0200

    Updates

---
+++ precache-manifest.e93a7e7d5629b2671db42b5ad17dadaa.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "843da7c0358ac6ff86d82ac26b74b954",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "5a9c03e94f13ac7ede7d",
+    "url": "/portfolio-site/static/css/main.b4f8e9b4.chunk.css"
+  },
+  {
+    "revision": "8a2da19e43df771ec1e0",
+    "url": "/portfolio-site/static/js/2.a6b6c12f.chunk.js"
+  },
+  {
+    "revision": "5a9c03e94f13ac7ede7d",
+    "url": "/portfolio-site/static/js/main.ac9c0e7d.chunk.js"
+  },
+  {
+    "revision": "12fc0976b19caa05f00a",
+    "url": "/portfolio-site/static/js/runtime~main.91922847.js"
+  },
+  {
+    "revision": "42310385f749074b95a6012499206d01",
+    "url": "/portfolio-site/static/media/cardman.42310385.svg"
+  },
+  {
+    "revision": "4cd529fe3f77ff746cc347e15b3b3499",
+    "url": "/portfolio-site/static/media/e_mail.4cd529fe.svg"
+  },
+  {
+    "revision": "e618bb41fde0ce29ae79a4b9965bbb46",
+    "url": "/portfolio-site/static/media/github.e618bb41.svg"
+  },
+  {
+    "revision": "3af095f806937b17eef850e4c1f6829b",
+    "url": "/portfolio-site/static/media/linkedin.3af095f8.svg"
+  },
+  {
+    "revision": "16bf23047466818ea81a130abc8f3a62",
+    "url": "/portfolio-site/static/media/my_photo.16bf2304.jpg"
+  },
+  {
+    "revision": "d9a03262e615a358318f8346f773caa6",
+    "url": "/portfolio-site/static/media/poznajmy-sie.d9a03262.svg"
+  },
+  {
+    "revision": "32a9a5cb8e1bf11df70964812d81beaa",
+    "url": "/portfolio-site/static/media/welcome-page-bg.32a9a5cb.jpg"
+  }
+]);
